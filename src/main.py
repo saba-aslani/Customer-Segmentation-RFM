8aab84d1d4a92882fad8f25dbc58428b0501abd6
saba-aslani <aslanisaba2@gmail.com>
2026-04-24 11:40:40 -0700

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+From extract import extract
+from transform import transform
+from load import load
+
+# Path to the dataset
+file_path = "SuperStoreOrders.csv"
+
+# Run ETL pipeline
+df = extract(file_path)
+rfm = transform(df)
+load(rfm)
+
+print("ETL pipeline executed successfully.")
